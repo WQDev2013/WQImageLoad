95f537a16cf01ff2feb32e0b56b50cc25b4282ce
chenweiqiang <EX-CHENWEIQIANG001@pingan.com.cn>
2018-08-22 09:24:25 +0800

    Initial Commit

---
+++ WQImageLoad/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WQImageLoad
+//
+//  Created by chenweiqiang on 2018/8/22.
+//  Copyright © 2018年 chenweiqiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
